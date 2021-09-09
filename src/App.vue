86ffdf7d067398416d3ec454ac22d26d1feb3d12
Brett Frable <bfrable@gmail.com>
2021-09-09 07:55:22 -0400

    updated package.json

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,26 @@
 <template>
-    <!-- header goes here -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <!-- Accounts header -->
-    <HeaderMain header="Accounts" />
-    <!-- Banking -->
-    <SectionBanking header="Banking" />
-    <!-- Credit Cards -->
-    <SectionCC header="Credit Cards" />
-    <!-- Loans -->
-    <SectionLoan header="Loans" />
-    <!-- Investing -->
-    <SectionInvest header="Investing" />
-    <!-- Relationship Manager -->
-    <SectionRM header="Relationship Manager" />
-    <!-- footer goes here -->
+    <div>
+        <!-- header goes here -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+        <!-- Accounts header -->
+        <TopBar />
+    </div>
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
-import HeaderMain from "./components/HeaderMain.vue";
-import SectionBanking from "./components/SectionBanking.vue";
-import SectionCC from "./components/SectionCC.vue";
-import SectionLoan from "./components/SectionLoan.vue";
-import SectionInvest from "./components/SectionInvest.vue";
-import SectionRM from "./components/SectionRM.vue";
+import TopBar from './components/TopBar.vue';
+
 export default {
     name: "App",
     components: {
-        //HelloWorld,
-        HeaderMain,
-        SectionBanking,
-        SectionCC,
-        SectionLoan,
-        SectionInvest,
-        SectionRM,
+        TopBar
     },
 };
-//expandable
-
-// const { RbcExpandable } = index;
-// var app = new Vue({
-//   el: "#app",
-//   name: 'expandable',
-//   components: {
-//     RbcExpandable
-//   }
-// });
-
-//icon
-
-//     export default {
-// 	name: 'icon',
-// 	components: {
-// 		RbcIcon
-// 	}
-// };
-
-//button
-
-// const { RbcButton } = index;
-// var app = new Vue({
-//   el: "#app",
-//   name: "button-example",
-//   components: { RbcButton },
-// });
-
-//caption
-
-//   const { RbcCaption } = index;
-// var app = new Vue({
-//   el: "#app",
-//   name: "caption-example",
-//   components: {
-//     RbcCaption
-//   }
-// });
-
-//card
-	// const { RbcCard } = index;
-	// var app = new Vue({
-	// 	el: "#app",
-	// 	name: "card-example",
-	// 	components: { RbcCard }
-	// });
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
---
+++ src/components/TopBar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<rbc-top-nav 
+			id="TopBar"
+			:items="links"
+			logo="cnb-rbc-integrated-reversed.svg"
+			logo-label="CNO"
+			logo-link="#"
+		/>
+	</div>
+</template>
+
+<script>
+import { RbcTopNav } from '../../public/index.umd.min.js'
+export default {
+	name: 'TopBar',
+	components: {
+		RbcTopNav
+	},
+	data() {
+		return {
+      links: [
+				{
+					name: 'Account Summary',
+				},
+				{
+					name: 'My Accounts',
+				},
+				{
+					name: 'Payments & Transfers',
+				},
+				{
+					name: 'Open an Account',
+				},
+				{
+					name: 'Sign Out',
+				}
+      ]
+		};
+	}
+};
+</script>
